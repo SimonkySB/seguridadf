50a2752860123caf6543f2e9edc28ff500b4c7d9
simonky <simonasb19@gmail.com>
2024-10-19 10:19:13 -0300

    Initial

---
+++ src/main/java/com/simonky/seguridadf/SeguridadfApplication.java
@@ -0,0 +1,13 @@
+package com.simonky.seguridadf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simonky/seguridadf/SeguridadfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonky.seguridadf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
